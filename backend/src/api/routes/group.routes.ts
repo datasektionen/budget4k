87a0e5e80187d9092d917402d0b814460b846d6b
Pontus <52171526+psoder@users.noreply.github.com>
2022-10-07 01:47:23 +0200

    Add default error responses

--- backend/src/api/routes/group.routes.ts
+++ backend/src/api/routes/group.routes.ts
@@ -14,19 +14,43 @@ const groupRouter = Router();
  * /v1/group:
  *  get:
  *    summary: Returns all the groups.
+ *    responses:
+ *      200:
+ *        description: A list containing all groups.
+ *        content: *groupContent
+ *    tags:
+ *      - Group
+ */
+groupRouter.get("/", getAllGroups);
+
+/**
+ * @openapi
+ * /v1/group:
+ *  post:
+ *    summary: Creates a new group.
+ *  tags:
+ *    - Group
+ */
+groupRouter.post("/", postGroup);
+
+/**
+ * @openapi
+ * /v1/group/{groupId}:
+ *  get:
+ *    summary: Returns a specific group
  *    produces:
  *     - application/json
  *    responses:
  *      200:
  *        description: A list containing all groups.
- *        content: *groupContent
+ *        content:
+ *            application/json:
+ *              schema:
+ *                $ref: "#/components/schemas/Group"
  *      500: Somthing went wrong.
  *    tags:
  *      - Group
  */
-groupRouter.get("/", getAllGroups);
-groupRouter.post("/", postGroup);
-
 groupRouter.get("/:groupId", getGroupById);
 groupRouter.put("/:groupId", putGroup);
 groupRouter.delete("/:groupId", deleteGroup);
